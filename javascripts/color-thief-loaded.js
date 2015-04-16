7ba1a2ecec023802b16f3b6afd68f88b6aff5a2c
TouNeko <touneko@gmail.com>
2015-04-16 16:56:52 +0800

    新增表單錯誤狀態＆調整首頁細節

--- javascripts/color-thief-loaded.js
+++ javascripts/color-thief-loaded.js
@@ -11,5 +11,14 @@ function loaded(id) {
   var colorThief = new ColorThief();
   var color = colorThief.getColor(img);
 
-  targetTag.parents('.travel-item').find('.enter').css('background-color', "rgb(" + color + ")");
+  var deepcolor = new Array();
+  deepcolor[0] = color[0] - 20;
+  deepcolor[1] = color[1] - 20;
+  deepcolor[2] = color[2] - 20;
+  if (deepcolor[0] < 0) { deepcolor[0] = 0; };
+  if (deepcolor[1] < 0) { deepcolor[1] = 0; };
+  if (deepcolor[2] < 0) { deepcolor[2] = 0; };
+  // console.log(deepcolor);
+
+  targetTag.parents('.travel-item').find('.enter').css('background-color', "rgb(" + deepcolor + ")");
 }
